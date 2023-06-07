e9aabd56ef335610103ff0a4fafbe900a2f28bf2
tilakved <tilakved3898@gmail.com>
2023-06-07 16:20:52 +0530

    7-6-23 | added flaticons, kticons, added toggle on left navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,66 +7,74 @@ import {RouterModule} from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  menuList: any = [
-    {
-      name: 'Dashboard',
-      subMenuList: ['Latest Updated', 'Layout Builder']
-    }, {
-      name: 'Features',
-      subMenuList: ['Bootstrap', 'Custom', 'Icons', 'Cards', 'Widgets', 'Calendar', 'Charts', 'Maps', 'Miscellaneous', 'Layout Builder']
-    }, {
-      name: 'Crud',
-      subMenuList: ['Form & Controls', 'KTDatatable', 'Datatable.net', 'File Upload']
-    }, {
-      name: 'Apps',
-      subMenuList: ['Users', 'Profile', 'Contacts', 'Chat', 'Projects', 'Support Center', 'Todo', 'Education', 'eCommerce', 'Inbox']
-    }, {
-      name: 'Pages',
-      subMenuList: ['Pricing Tables', 'Wizards', 'Invoices', 'Login', 'Classic Login', 'Error Pages']
-    },
-  ];
+  // menuList: any = [
+  //   {
+  //     name: 'Dashboard',
+  //     subMenuList: ['Latest Updated', 'Layout Builder']
+  //   }, {
+  //     name: 'Features',
+  //     subMenuList: ['Bootstrap', 'Custom', 'Icons', 'Cards', 'Widgets', 'Calendar', 'Charts', 'Maps', 'Miscellaneous', 'Layout Builder']
+  //   }, {
+  //     name: 'Crud',
+  //     subMenuList: ['Form & Controls', 'KTDatatable', 'Datatable.net', 'File Upload']
+  //   }, {
+  //     name: 'Apps',
+  //     subMenuList: ['Users', 'Profile', 'Contacts', 'Chat', 'Projects', 'Support Center', 'Todo', 'Education', 'eCommerce', 'Inbox']
+  //   }, {
+  //     name: 'Pages',
+  //     subMenuList: ['Pricing Tables', 'Wizards', 'Invoices', 'Login', 'Classic Login', 'Error Pages']
+  //   },
+  // ];
 
   selectedMenu: any = '';
-  selectedSubMenu: any = '';
-  rightselection: any = 'right-panel';
-  selectedTab: any = 'alert';
+  selectedSubMenu_1: any = '';
+  selectedSubMenu_2:any  = '';
+  selectedSubMenu_3:any  = '';
 
   ngOnInit(): void {
-    // this.selectedMenu = 'Dashboard';
   }
 
   toggleMenu(item: any) {
-    if (this.selectedMenu == item.name) {
+    if (this.selectedMenu == item) {
       this.selectedMenu = ''
     } else {
-      setTimeout(() => {
-        this.selectedMenu = item.name
-      }, 100)
+      // setTimeout(() => {
+        this.selectedMenu = item
+      // }, 100)
     }
-
+    this.selectedSubMenu_1 = ''
+    this.selectedSubMenu_2 = ''
+    this.selectedSubMenu_3 = ''
   }
-
-  selectSubMenuItem(item: any) {
-    this.selectedSubMenu = item
+  toggleSubMenu_1(item: any) {
+    if (this.selectedSubMenu_1 == item) {
+      this.selectedSubMenu_1 = ''
+    } else {
+      // setTimeout(() => {
+        this.selectedSubMenu_1 = item
+      // }, 100)
+    }
+    this.selectedSubMenu_2 = ''
+    this.selectedSubMenu_3 = ''
   }
-
-  closeMenu($event: any) {
-    this.selectedMenu = ''
+  toggleSubMenu_2(item: any) {
+    if (this.selectedSubMenu_2 == item) {
+      this.selectedSubMenu_2 = ''
+    } else {
+      // setTimeout(() => {
+        this.selectedSubMenu_2 = item
+      // }, 100)
+    }
+    this.selectedSubMenu_3 = ''
   }
-
-  selectRightItem(type: string) {
-    if(this.rightselection == type){
-      this.rightselection = ''
-    }else{
-      this.rightselection = type
+  toggleSubMenu_3(item: any) {
+    if (this.selectedSubMenu_3 == item) {
+      this.selectedSubMenu_3 = ''
+    } else {
+      // setTimeout(() => {
+        this.selectedSubMenu_3 = item
+      // }, 100)
     }
   }
 
-  selectedNotifyTab(type: string) {
-    if(this.selectedTab == type){
-      this.selectedTab = ''
-    }else{
-      this.selectedTab = type
-    }
-  }
 }
